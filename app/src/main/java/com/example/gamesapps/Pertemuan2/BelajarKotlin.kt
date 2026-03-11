26f3a87ee4c5a02257e42f6c9caaca06cdc45ea9
Arsyad <arsyad24si@mahasiswa.pcr.ac.id>
2026-03-11 16:02:53 +0700

    first commit

---
+++ app/src/main/java/com/example/gamesapps/Pertemuan2/BelajarKotlin.kt
@@ -0,0 +1,46 @@
+package com.example.gamesapps.Pertemuan2
+fun main() {
+    println("hai rekan reksn...")
+    println("selamat datang di bahassa pemrogramamn Kotlin")
+
+    println("================")
+
+    var angka = 15
+    println("hasil dari 15 + 10 = $ {angka + 10}")
+
+    val nilaiInt= 10000
+    val nilaiDoiuble = 100.003
+    val nilaiFloat = 1000.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDoiuble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("=======STRING=======")
+    val huruf = 'a'
+    println("ini penggunaan karakter '$huruf")
+
+    val nilaiString = 'a'
+    println("hallo $nilaiString!\nApa kabar?")
+
+    println("=======KONDISI=======")
+
+    val nilai = 10
+    if(nilai<0)
+        println("bilangan negatif")
+    else {
+        if(nilai%2 == 0)
+            println("bilangan genap")
+        else
+            println("bilangan ganjil")
+    }
+
+    println("=======PERULSANGAN=======")
+    val kampusKu: Array<String> = arrayOf("kampus","politekni", "caltex", "riau")
+    for (kampus: String in kampusKu) {
+        println(kampus)
+    }
+
+
+
+}
