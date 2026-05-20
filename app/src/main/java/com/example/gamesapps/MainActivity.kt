1afc67e4e400f343be9692dc20691a244d73ea2d
Arsyad <arsyad24si@mahasiswa.pcr.ac.id>
2026-05-20 15:35:58 +0700

    Pertemuan10

--- app/src/main/java/com/example/gamesapps/MainActivity.kt
+++ app/src/main/java/com/example/gamesapps/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.gamesapps
 
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,9 +26,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Memanggil ID btnWeb yang sudah disesuaikan di XML
         binding.btnWeb.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
-
 
             intent.putExtra("name", "Politeknik Caltex Riau")
             intent.putExtra("from", "Rumbai")
